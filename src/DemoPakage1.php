d73a2305d1a08e2f820ed3c76e349897fa274033
wojianduanfa <404599132@qq.com>
2025-04-25 20:12:08 +0800

    fix

--- src/DemoPakage1.php
+++ src/DemoPakage1.php
@@ -4,15 +4,15 @@ namespace wojianduanfa\DemoPakage1;
 
 class DemoPakage1
 {
-    protected $config;
 
-    public function __construct($config)
+
+    public function __construct()
     {
-        $this->config = $config;
+
     }
 
     public function say()
     {
-        return $this->config['message'];
+        return config('demopakage1.message');
     }
 }
